7a8d500aea129d773268619ee83eff396868a5d3
PVSKR <skr.polu@hotmail.com>
2024-05-06 16:26:40 +0530

    server of client side data base  completed.

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+const multer = require("multer");
+
+const app = express();
+app.use(cors());
+const PORT = 5038;
+
+const CONNECTION_STRING =
+  "mongodb+srv://admin:Yrskrmsr@cluster0.nwljkhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DATABASE_NAME = "employeedb";
+let database;
+
+app.listen(PORT, () => {
+  MongoClient.connect(CONNECTION_STRING, (error, client) => {
+    if (error) {
+      console.error("Error connecting to MongoDB:", error);
+      return;
+    }
+    database = client.db(DATABASE_NAME);
+    console.log("MongoDB Connection Successful");
+  });
+});
