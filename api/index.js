971ac6e38be656336145d230b83bcefdd3572eea
PVSKR <skr.polu@hotmail.com>
2024-05-06 17:01:43 +0530

    server side  post and delete methods created

--- api/index.js
+++ api/index.js
@@ -22,3 +22,56 @@ app.listen(PORT, () => {
     console.log("MongoDB Connection Successful");
   });
 });
+app.use(express.json());
+
+app.get("/api/employee/GetInfo", (req, res) => {
+  database
+    .collection("employeecollection")
+    .find({})
+    .toArray((error, result) => {
+      if (error) {
+        console.error("Error fetching notes:", error);
+        res.status(500).json({ error: "Internal server error" });
+        return;
+      }
+      res.json(result);
+    });
+});
+
+app.post("/api/employee/AddEmployees", multer().none(), (req, res) => {
+  const newNote = req.body.newNotes;
+  database.collection("todoappcollect").countDocuments({}, (error, count) => {
+    if (error) {
+      console.error("Error counting documents:", error);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    const newEmployeeObject = {
+      id: (count + 1).toString(),
+      name: newEmployee,
+      salary: newEmployee,
+    };
+    database.collection("employeecollect").insertOne(newNoteObject, (error) => {
+      if (error) {
+        console.error("Error adding employee:", error);
+        res.status(500).json({ error: "Internal server error" });
+        return;
+      }
+      res.json({ message: "Added Successfully" });
+    });
+  });
+});
+
+app.delete("/api/employee/DeleteEmplouees", (req, res) => {
+  const employeeId = req.query.id;
+  database
+    .collection("employeecollect")
+    .deleteOne({ id: employeeIdId }, (error) => {
+      if (error) {
+        console.error("Error deleting employee:", error);
+        res.status(500).json({ error: "Internal server error" });
+        return;
+      }
+      res.json({ message: "Deleted Successfully" });
+    });
+});
