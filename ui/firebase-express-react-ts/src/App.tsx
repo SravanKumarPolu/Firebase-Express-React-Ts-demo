efe111d6adc135a8de66d36544efda53974b353b
PVSKR <skr.polu@hotmail.com>
2024-05-06 17:36:59 +0530

    Employee information retrieved from the database
    In this project mainly focus one the firebase database  and  react and express.js and ts .

--- ui/firebase-express-react-ts/src/App.tsx
+++ ui/firebase-express-react-ts/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+interface Employee {
+  id: string;
+  name: string;
+  salary: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [employees, setEmployees] = useState<Employee[]>([]);
 
+  const API_URL = "http://localhost:5038/";
+  useEffect(() => {
+    refreshEmpoyees();
+  }, []);
+  const refreshEmpoyees = async () => {
+    try {
+      const response = await fetch(API_URL + "api/employee/getinfo");
+      const data = await response.json();
+      setEmployees(data);
+    } catch (error) {
+      console.error("Error fetching notes:", error);
+    }
+  };
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Firebase-Express-React-Ts-demo</h1>
+        <h2> Employees Info</h2>
+        {employees.map((employee, index) => (
+          <p key={index}>
+            <span className="flex flex-col xs:flex-col md:flex-row px-10 items-center justify-center text-center gap-5">
+              <b className=" flex gap-10 text-left h-10  w-[20rem] ">
+                {employee.name}
+                <span>₹{employee.salary}/-</span>
+              </b>
+            </span>
+          </p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
